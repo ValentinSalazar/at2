097913e599b5c558e1c344b29519deda9477f3b5
ValentinPC <facundev@outlook.com>
2024-02-17 11:32:11 -0300

    to vercel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,vue}'],
 	theme: {
 		screens:{
 			'xs': {'min': '320px', 'max':'380px'},
